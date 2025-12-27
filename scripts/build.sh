fe3478d5f0ede1e701a8a2a9ca8f8fe082f083d6
Admin <admin@costareis.eu>
2025-12-27 23:09:55 +0000

    fixed build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,10 @@ set -eu
 IMAGE="architectstore/certbot-hostinger"
 TAG_DATE="$(date +%d%m%Y)"
 
-docker build \
+docker buildx create --name multiarch --driver docker-container --use
+docker buildx inspect --bootstrap
+
+docker buildx build --platform linux/amd64,linux/arm64 \
   -t "${IMAGE}:latest" \
   -t "${IMAGE}:${TAG_DATE}" \
-  .
+  --push .
